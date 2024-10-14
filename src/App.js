b3c90524eb46d414ae9917d379167ca6a82c7507
AndoniAlvarez <andonialvarez3@gmail.com>
2024-10-14 19:31:24 +0200

    Pending icons of links

--- src/App.js
+++ src/App.js
@@ -12,66 +12,82 @@ import NewArticleModal from './components/newModal';
 
 
 function App() {
-    const [showModal, setShowModal] = useState(false);
-    const allCategories = [
+	const [showModal, setShowModal] = useState(false);
+	const allCategories = [
 
-        ...new Set(data.map(article => article.category)),
-    ];
+		...new Set(data.map(article => article.category)),
+	];
 
-    const [categories, setCategories] = useState(allCategories);
-    const [articles, setArticles] = useState(data);
+	const [categories, setCategories] = useState(allCategories);
+	const [articles, setArticles] = useState(data);
 
-    const filterCategory = (category) => {
-        if (category === 'Todas') {
-            setArticles(data)
-            return;
-        }
+	const filterCategory = (category) => {
+		if (category === 'Todas') {
+			setArticles(data)
+			return;
+		}
 
-        const filteredData = data.filter(article => article.category === category);
-        setArticles(filteredData)
-    }
+		const filteredData = data.filter(article => article.category === category);
+		setArticles(filteredData)
+	}
 
-    return (
-        <Router>
-            <div className='title'>
-                <h1>
-                    Termita <span>Blog</span>
-                </h1>
-                <img
-                    src='https://i0.wp.com/www.cinemanet.info/wp-content/uploads/2019/05/Portada_Manual_Critica_Cine.jpg?fit=1000%2C554&ssl=1'
-                    alt='imagen-header'
-                />
-            </div>
+	return (
+		<Router>
+			<div className='title'>
+				<h1>
+					Termita <span>Blog</span>
+				</h1>
+				<img
+					src='https://i0.wp.com/www.cinemanet.info/wp-content/uploads/2019/05/Portada_Manual_Critica_Cine.jpg?fit=1000%2C554&ssl=1'
+					alt='imagen-header'
+				/>
+			</div>
 
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Inicio</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About Us</Link> {/* Enlace a la página de About */}
-                    </li>
-                </ul>
-            </nav>
+			<div className='links'>
+				<div className='links-inicio'>
+					<Link to="/">
+						<div className="image-container">
+							<img src='https://images.unsplash.com/reserve/LJIZlzHgQ7WPSh5KVTCB_Typewriter.jpg?q=80&w=1992&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt="Inicio"></img>
+							<p className="image-text">Inicio</p> {/* Frase dentro de la imagen */}
+						</div>
+					</Link>
+				</div>
+				<div className='links-inicio'>
+					<Link to="/about">
+						<div className="image-container">
+							<img src='https://i0.wp.com/www.cinemanet.info/wp-content/uploads/2019/05/Portada_Manual_Critica_Cine.jpg?fit=1000%2C554&ssl=1' alt="About"></img>
+							<p className="image-text">Acerca de</p> {/* Frase dentro de la imagen */}
+						</div>
+					</Link>
+				</div>
+				<div className='links-inicio'>
+					<Link to="/about">
+						<div className="image-container">
+							<img src='https://i0.wp.com/www.cinemanet.info/wp-content/uploads/2019/05/Portada_Manual_Critica_Cine.jpg?fit=1000%2C554&ssl=1' alt="About"></img>
+							<p className="image-text">Acerca de</p> {/* Frase dentro de la imagen */}
+						</div>
+					</Link>
+				</div>
+			</div>
 
-            <Routes>
-                <Route path="/" element={
-                    <>
-                        <ButtonList categories={categories} filterCategory={filterCategory} />
-                        <ArticleList articles={articles} />
-                        <MovieCarousel />
-                        <h1>Añadir nuevo artículo</h1>
-                        <button style={{ padding: "10px", color: "black", background: '#e18255', borderRadius: '15px' }} onClick={() => setShowModal(true)}>Añadir nuevo artículo</button>
-                        <NewArticleModal showModal={showModal} setShowModal={setShowModal} />
-                    </>
-                } />
+			<Routes>
+				<Route path="/" element={
+					<>
+						<ButtonList categories={categories} filterCategory={filterCategory} />
+						<ArticleList articles={articles} />
+						<MovieCarousel />
+						<h1>Añadir nuevo artículo</h1>
+						<button className='modal-button' onClick={() => setShowModal(true)}>Añadir nuevo artículo</button>
+						<NewArticleModal showModal={showModal} setShowModal={setShowModal} />
+					</>
+				} />
 
-                <Route path="/about" element={<About />} /> {/* Ruta para la página About */}
-            </Routes>
+				<Route path="/about" element={<About />} /> {/* Ruta para la página About */}
+			</Routes>
 
-            <Footer />
-        </Router>
-    );
+			<Footer />
+		</Router>
+	);
 }
 
 export default App;
